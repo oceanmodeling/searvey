a5d098517958f28f4e232a38f247621f63003bd9
zacharyburnett <zachary.r.burnett@gmail.com>
2022-04-18 13:57:47 -0400

    remove serial mark (it is no longer needed)

--- tests/erddap_test.py
+++ tests/erddap_test.py
@@ -15,7 +15,6 @@ def test_openurl_wrong_url_raises() -> None:
     assert "The requested URL <code>/gibberish</code> was not found on this server" in str(exc)
 
 
-@pytest.mark.serial
 def test_openurl_timeout_raises() -> None:
     with pytest.raises(ReadTimeout) as exc:
         erddap.urlopen(
