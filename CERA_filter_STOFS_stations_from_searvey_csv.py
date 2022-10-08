cb21d38135886a7c0528f2c54f778e703f9f5430
Panos Mavrogiorgos <pmav99@gmail.com>
2022-10-08 19:42:24 +0300

    pre-commit: Run pre-commit hooks on Carola's scripts

--- CERA_filter_STOFS_stations_from_searvey_csv.py
+++ CERA_filter_STOFS_stations_from_searvey_csv.py
@@ -1,78 +1,83 @@
-# Coastal Emergency Risks Assessment (CERA)	
+# Coastal Emergency Risks Assessment (CERA)
 # =============================================================================
 # Copyright(c) 2006-2022 Carola Kaiser (ckaiser <at> cct.lsu.edu)
-
 # script to filter the NOAA Searvey data for CERA STOFS
-
-import os, sys
-import CERA_get_active_stations
 import datetime
+import os
+import re
+import sys
+
 import pandas as pd
-import re 
+
+import CERA_get_active_stations
 
 ################################################################################
 def get_csv():
 
-  # read searvey data and return geopandas geodataframe object
+    # read searvey data and return geopandas geodataframe object
 
-  # COOPS stations, status = active
-#  stations_coops = CERA_get_active_stations.get_coops_stations_activity()
-  #IOC stations, actve within the last 30 days
-  stations_ioc = CERA_get_active_stations.get_ioc_stations_activity(activity_threshold=datetime.timedelta(days=30))
+    # COOPS stations, status = active
+    #  stations_coops = CERA_get_active_stations.get_coops_stations_activity()
+    # IOC stations, actve within the last 30 days
+    stations_ioc = CERA_get_active_stations.get_ioc_stations_activity(
+        activity_threshold=datetime.timedelta(days=30)
+    )
 
-  # convert to geojson
-#  coops = stations_coops.to_json()
-#  f = open("CERA_searvey_coops_all.json", "w")
-#  f.write(coops)
-#  f.close()
+    # convert to geojson
+    #  coops = stations_coops.to_json()
+    #  f = open("CERA_searvey_coops_all.json", "w")
+    #  f.write(coops)
+    #  f.close()
 
-  ioc = stations_ioc.to_csv()
-  f = open("CERA_searvey_ioc_all.csv", "w")
-  f.write(ioc)
-  f.close()
+    ioc = stations_ioc.to_csv()
+    f = open("CERA_searvey_ioc_all.csv", "w")
+    f.write(ioc)
+    f.close()
 
-  # filter active stations
-#  coops_active = stations_coops[stations_coops['is_active']]   # comes back as True/False
-  ioc_active = stations_ioc[stations_ioc['is_active']]
- 
-  # convert to geojson
-#  coops_active = coops_active.to_json()
-#  f = open("CERA_searvey_coops_active.json", "w")
-#  f.write(coops_active)
-#  f.close()
+    # filter active stations
+    #  coops_active = stations_coops[stations_coops['is_active']]   # comes back as True/False
+    ioc_active = stations_ioc[stations_ioc["is_active"]]
 
-  ################################################
-  # write original geopandas into csv
-  f = open("CERA_searvey_ioc_active_all.csv", "w", encoding = 'latin1')
-  # original fieldnames = ['', 'provider','provider_id','country','location','lon','lat','is_active','start_date','last_observation','geometry']
-  f.write(ioc_active.to_csv())
-  f.close()
+    # convert to geojson
+    #  coops_active = coops_active.to_json()
+    #  f = open("CERA_searvey_coops_active.json", "w")
+    #  f.write(coops_active)
+    #  f.close()
 
-  ##################################################
-  # remove US stations for IOC (assume they are duplicates from COOPS)
-  df =  ioc_active[ioc_active.country != "USA"] 
-  f1 = open("CERA_searvey_ioc_active_no_USA.csv", "w", encoding = 'latin1')
-  f1.write(df.to_csv())
-  f1.close()
+    ################################################
+    # write original geopandas into csv
+    f = open("CERA_searvey_ioc_active_all.csv", "w", encoding="latin1")
+    # original fieldnames = ['', 'provider','provider_id','country','location','lon','lat','is_active','start_date','last_observation','geometry']
+    f.write(ioc_active.to_csv())
+    f.close()
 
-  ##################################################
-  # make pandas dataframe from csv
-  df = pd.read_csv('CERA_searvey_ioc_active_no_USA.csv', encoding = 'latin1')
+    ##################################################
+    # remove US stations for IOC (assume they are duplicates from COOPS)
+    df = ioc_active[ioc_active.country != "USA"]
+    f1 = open("CERA_searvey_ioc_active_no_USA.csv", "w", encoding="latin1")
+    f1.write(df.to_csv())
+    f1.close()
 
-  #df["stationid"] = df.apply(lambda x:'STOFS_%s' % (x['provider_id']),axis=1)  # keep provider_id, STOFS_ will be added in CERA DB script
-  # combine 'location' and 'country' to 'stationname'
-  df["stationname"] = df.apply(lambda x:'%s (%s)' % (x['location'],x['country']),axis=1)
-  # keep only specific columns
-  keep_col = ['provider_id','stationname','lon','lat']
-  f2 = df[keep_col]
+    ##################################################
+    # make pandas dataframe from csv
+    df = pd.read_csv("CERA_searvey_ioc_active_no_USA.csv", encoding="latin1")
 
-  f2.to_csv("CERA_searvey_ioc_active_no USA_for_CERA_DB.csv", encoding = 'latin1', index=False)  
+    # df["stationid"] = df.apply(lambda x:'STOFS_%s' % (x['provider_id']),axis=1)  # keep provider_id, STOFS_ will be added in CERA DB script
+    # combine 'location' and 'country' to 'stationname'
+    df["stationname"] = df.apply(lambda x: "%s (%s)" % (x["location"], x["country"]), axis=1)
+    # keep only specific columns
+    keep_col = ["provider_id", "stationname", "lon", "lat"]
+    f2 = df[keep_col]
 
-  # filter inactive stations
+    f2.to_csv("CERA_searvey_ioc_active_no USA_for_CERA_DB.csv", encoding="latin1", index=False)
+
+    # filter inactive stations
+
+
 #  coops_inactive = stations_coops[~stations_coops['is_active']]   # comes back as True/False
 #  ioc_inactive = stations_ioc[~stations_ioc['is_active']]
 
-  # convert to geojson
+# convert to geojson
 #  coops_inactive = coops_inactive.to_json()
 #  f = open("CERA_searvey_coops_inactive.json", "w")
 #  f.write(coops_inactive)
@@ -84,12 +89,13 @@ def get_csv():
 #  f.close()
 
 # do main work
-if __name__ == '__main__':
-  try:
-    get_csv()
+if __name__ == "__main__":
+    try:
+        get_csv()
 
-  except Exception as e:
-    import traceback, sys
-    tb = sys.exc_info()[2]
-    tbstr = traceback.format_tb(tb)
-    print("An error occurred: %s\n%s\n" % (str(e), tbstr))
+    except Exception as e:
+        import traceback, sys
+
+        tb = sys.exc_info()[2]
+        tbstr = traceback.format_tb(tb)
+        print("An error occurred: %s\n%s\n" % (str(e), tbstr))
--- CERA_filter_STOFS_stations_from_searvey_json.py
+++ CERA_filter_STOFS_stations_from_searvey_json.py
@@ -1,71 +1,75 @@
-# Coastal Emergency Risks Assessment (CERA)	
+# Coastal Emergency Risks Assessment (CERA)
 # =============================================================================
 # Copyright(c) 2006-2022 Carola Kaiser (ckaiser <at> cct.lsu.edu)
-
 # script to filter the NOAA Searvey data for CERA STOFS
-
-import os, sys
-import CERA_get_active_stations
 import datetime
+import os
+import sys
+
+import CERA_get_active_stations
 
 ################################################################################
 def get_geojson():
 
-  # read searvey data and return geopandas geodataframe object
+    # read searvey data and return geopandas geodataframe object
 
-  # COOPS stations, status = active
-  stations_coops = CERA_get_active_stations.get_coops_stations_activity()
-  #IOC stations, actve within the last 2 days
-  stations_ioc = CERA_get_active_stations.get_ioc_stations_activity(activity_threshold=datetime.timedelta(days=2))
+    # COOPS stations, status = active
+    stations_coops = CERA_get_active_stations.get_coops_stations_activity()
+    # IOC stations, actve within the last 2 days
+    stations_ioc = CERA_get_active_stations.get_ioc_stations_activity(
+        activity_threshold=datetime.timedelta(days=2)
+    )
 
-  # convert to geojson
-  coops = stations_coops.to_json()
-  f = open("CERA_searvey_coops_all.json", "w")
-  f.write(coops)
-  f.close()
+    # convert to geojson
+    coops = stations_coops.to_json()
+    f = open("CERA_searvey_coops_all.json", "w")
+    f.write(coops)
+    f.close()
 
-  ioc = stations_ioc.to_json()
-  f = open("CERA_searvey_ioc_all.json", "w")
-  f.write(ioc)
-  f.close()
+    ioc = stations_ioc.to_json()
+    f = open("CERA_searvey_ioc_all.json", "w")
+    f.write(ioc)
+    f.close()
 
-  # filter active stations
-  coops_active = stations_coops[stations_coops['is_active']]   # comes back as True/False
-  ioc_active = stations_ioc[stations_ioc['is_active']]
+    # filter active stations
+    coops_active = stations_coops[stations_coops["is_active"]]  # comes back as True/False
+    ioc_active = stations_ioc[stations_ioc["is_active"]]
 
-  # convert to geojson
-  coops_active = coops_active.to_json()
-  f = open("CERA_searvey_coops_active.json", "w")
-  f.write(coops_active)
-  f.close()
+    # convert to geojson
+    coops_active = coops_active.to_json()
+    f = open("CERA_searvey_coops_active.json", "w")
+    f.write(coops_active)
+    f.close()
 
-  ioc_active = ioc_active.to_json()
-  f = open("CERA_searvey_ioc_active.json", "w")
-  f.write(ioc_active)
-  f.close()
+    ioc_active = ioc_active.to_json()
+    f = open("CERA_searvey_ioc_active.json", "w")
+    f.write(ioc_active)
+    f.close()
 
-  # filter inactive stations
-  coops_inactive = stations_coops[~stations_coops['is_active']]   # comes back as True/False
-  ioc_inactive = stations_ioc[~stations_ioc['is_active']]
+    # filter inactive stations
+    coops_inactive = stations_coops[~stations_coops["is_active"]]  # comes back as True/False
+    ioc_inactive = stations_ioc[~stations_ioc["is_active"]]
 
-  # convert to geojson
-  coops_inactive = coops_inactive.to_json()
-  f = open("CERA_searvey_coops_inactive.json", "w")
-  f.write(coops_inactive)
-  f.close()
+    # convert to geojson
+    coops_inactive = coops_inactive.to_json()
+    f = open("CERA_searvey_coops_inactive.json", "w")
+    f.write(coops_inactive)
+    f.close()
 
-  ioc_inactive = ioc_inactive.to_json()
-  f = open("CERA_searvey_ioc_inactive.json", "w")
-  f.write(ioc_inactive)
-  f.close()
+    ioc_inactive = ioc_inactive.to_json()
+    f = open("CERA_searvey_ioc_inactive.json", "w")
+    f.write(ioc_inactive)
+    f.close()
+
 
 # do main work
-if __name__ == '__main__':
-  try:
-    get_geojson()
+if __name__ == "__main__":
+    try:
+        get_geojson()
 
-  except Exception as e:
-    import traceback, sys
-    tb = sys.exc_info()[2]
-    tbstr = traceback.format_tb(tb)
-    print("An error occurred: %s\n%s\n" % (str(e), tbstr))
+    except Exception as e:
+        import traceback, sys
+
+        tb = sys.exc_info()[2]
+        tbstr = traceback.format_tb(tb)
+        print("An error occurred: %s\n%s\n" % (str(e), tbstr))
