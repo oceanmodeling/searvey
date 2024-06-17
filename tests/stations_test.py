32bfb2f39f01807a5b803a3528df72f88150015c
abdu558 <arefabdu55@gmail.com>
2024-06-17 20:21:41 +0100

    Add NDBC station testing

--- tests/stations_test.py
+++ tests/stations_test.py
@@ -22,11 +22,12 @@ def test_get_stations_specify_providers():
     ioc_provider = stations.get_stations(activity_threshold=threshold, providers=stations.Provider.IOC)
     coops_provider = stations.get_stations(activity_threshold=threshold, providers=stations.Provider.COOPS)
     usgs_provider = stations.get_stations(activity_threshold=threshold, providers=stations.Provider.USGS)
+    ndbc_provider = stations.get_stations(providers=stations.Provider.NDBC)
     all_providers = stations.get_stations(activity_threshold=threshold, providers=stations.Provider.ALL)
     multiple_providers = stations.get_stations(
         activity_threshold=threshold,
-        providers=[stations.Provider.COOPS, stations.Provider.IOC, stations.Provider.USGS],
+        providers=[stations.Provider.COOPS, stations.Provider.IOC, stations.Provider.USGS, stations.Provider.NDBC],
     )
 
-    assert len(ioc_provider) + len(coops_provider) + len(usgs_provider) == len(all_providers)
+    assert len(ioc_provider) + len(coops_provider) + len(usgs_provider) + len(ndbc_provider) == len(all_providers)
     assert len(multiple_providers) == len(all_providers)
